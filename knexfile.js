068c697b6d9cde79794de6a96faaf7e9bc7aa0c3
Andrew Santos <andrewsan321@gmail.com>
2025-06-22 15:07:39 +1000

    update database connection to db deployed on avien

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,10 @@
 module.exports = {
   client: 'mysql2',
   connection: {
-    host: '127.0.0.1',
-    port: 3306,
-    database: 'movies',
-    user: 'root',
-    password: 'Cab230!'
+    host: 'mysql-movie-db-filmary-movies-db-filmary.b.aivencloud.com', // from Aiven
+    port: 12392, // from Aiven
+    database: 'movies', 
+    user: 'avnadmin', // from Aiven
+    password: 'AVNS_Ggi5nCMypEVtKSABm2_' // replace with the actual password
   }
 };

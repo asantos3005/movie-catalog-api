17613f664ed819f8426ffe1f8bdef54659a791c7
Andrew Santos <andrewsan321@gmail.com>
2025-06-22 15:20:19 +1000

    Changed hardcoded knex connections to use env vars and untrack env file

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,10 @@
 module.exports = {
   client: 'mysql2',
   connection: {
-    host: 'mysql-movie-db-filmary-movies-db-filmary.b.aivencloud.com', // from Aiven
-    port: 12392, // from Aiven
-    database: 'movies', 
-    user: 'avnadmin', // from Aiven
-    password: 'AVNS_Ggi5nCMypEVtKSABm2_' // replace with the actual password
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
   }
 };
